23880ba46b5d38981dfa6884d282b5bd67fb8f9e
Rob Kaufman <rob@notch8.com>
2017-10-03 13:27:03 -0700

    Revert "Add sequelizerc and Add Book skeleton"

--- literato-frontend/src/App.js
+++ literato-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import AddNewBook from './components/add-book'
+import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,9 +7,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to Literato</h1>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <AddNewBook />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
