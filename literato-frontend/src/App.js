3cde5fcfdb8a668e7c55390e988cfe623e0b53b3
learn <learn@learn18.local>
2017-10-03 16:34:16 -0700

    WIP: added search component that works with google books api

--- literato-frontend/src/App.js
+++ literato-frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import AddNewBook from './components/add-book'
+import SearchForBook from './components/search-add-book'
 import './App.css';
 
 class App extends Component {
@@ -13,6 +14,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+		<SearchForBook />
       </div>
     );
   }
