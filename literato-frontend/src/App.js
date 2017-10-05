1d55a685baf538825a6f99056a0fc582d7ad23d3
learn <learn@learn12.local>
2017-10-05 13:22:20 -0700

    Connected add a book to google api

--- literato-frontend/src/App.js
+++ literato-frontend/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import SearchToAdd from './components/googleComponents/search'
-import AddNewBook from './components/add-book'
 import UserBookList from './components/user-book-list'
 import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom'
 import './App.css';
@@ -26,35 +25,7 @@ class App extends Component {
     })
   }
 
-  handleNewBook(params){
-    fetch(`${this.state.apiUrl}/books`,
-    {
-      body: JSON.stringify(params),
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      method: 'POST'
-    }
-  )
-    .then((rawResponse) => {
-      console.log(rawResponse)
-      return rawResponse.json()
-    })
-    .then((parsedResponse) => {
-      if(parsedResponse.errors) {
-        this.setState({errors: parsedResponse.errors})
-      }else{
-        const books = Object.assign([], this.state.books)
-        books.push(parsedResponse.book)
-        this.setState(
-          {
-            books: books,
-            errors: null,
-            newBookSuccess: true
-        })
-      }
-    })
-  }
+
 
   render() {
     return (
@@ -66,12 +37,8 @@ class App extends Component {
           <SearchToAdd />
 
           <UserBookList books={this.state.books} />
-          <AddNewBook onSubmit={this.handleNewBook.bind(this)}/>
           {this.state.newBookSuccess && <Redirect to='/books'/>}
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </div>
+          </div>
       </Router>
     );
   }
