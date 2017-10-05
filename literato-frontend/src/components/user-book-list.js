fa7a35c0545d00fa843fae157efd69cd23f84c76
learn <learn@learn14.local>
2017-10-05 15:37:22 -0700

    delete book button works

--- literato-frontend/src/components/user-book-list.js
+++ literato-frontend/src/components/user-book-list.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react'
 
 class UserBookList extends React.Component {
+  constructor(props) {
+    super(props)
+      this.state = {
+        apiUrl: 'http://localhost:3000'
+      }
+  }
+
+deleteBook(id) {
+  fetch(`${this.state.apiUrl}/books/destroy`,
+  {
+    body: JSON.stringify({id: id}),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'POST'
+  }
+)
+  .then((rawResponse) => {
+    return rawResponse.json()
+  })
+  .then((parsedResponse) => {
+      console.log(parsedResponse)
+  })
+}
 
   render() {
-    console.log(this.props)
+    console.log(this.props.books)
     return(
       <div>
         <h1>My Books</h1>
@@ -14,6 +38,11 @@ class UserBookList extends React.Component {
                 <div>
                   <h4 className="book-title">{book.title}</h4>
                   <h5 className="book-authors">{book.authors}</h5>
+                  <h5 className="book-description">{book.description}</h5>
+                  <h5>{book.id}</h5>
+                  <button onClick={this.deleteBook.bind(this, book.id)}>
+                  Delete Book
+                  </button>
                 </div>
               </li>
             )
