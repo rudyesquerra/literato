8fd9fd81c95f0b861eeeabe4010a2d067b483a97
learn <learn@learn12.local>
2017-10-16 10:51:55 -0700

    Book search results now clear when user clicks the Add Book to Collection button

--- literato-frontend/src/reducers/booksReducer.js
+++ literato-frontend/src/reducers/booksReducer.js
@@ -2,7 +2,8 @@ const initialState = {
     deleteBookSuccess: false,
     books: [],
     errors: null,
-    newBookSuccess: false
+    newBookSuccess: false,
+    searchResults: []
 }
 
 export default (currentState=initialState, action) =>{
@@ -47,7 +48,19 @@ export default (currentState=initialState, action) =>{
                 {
                     books: action.payload,
                     newBookSuccess: true,
-                    errors: null
+                    errors: null,
+                    searchResults: []
+                }
+            )
+            break
+        }
+        case("BOOK_SEARCH"):{
+            newState = Object.assign(
+                {},
+                currentState,
+                {
+                    searchResults: action.payload,
+                    errors: "AHHHH"
                 }
             )
             break
