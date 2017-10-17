94276f1e2fa214e814bf68fe274ad9725557c9ef
learn <learn@learn18.local>
2017-10-17 09:52:13 -0700

    added new user and book table, and get api endpoint

---
+++ literato-backend/seeds/20171016213356-Request.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+      return queryInterface.bulkInsert('Requests',
+            [
+              {
+                user1Id: 1,
+                user2Id: 2,
+                book1Id: 1,
+                book2Id: 2,
+                createdAt: new Date(),
+                updatedAt: new Date()
+              },
+              {
+                user1Id: 1,
+                user2Id: 2,
+  //              book1: ,
+                book2Id: 3,
+                createdAt: new Date(),
+                updatedAt: new Date()
+              },
+            ])
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
